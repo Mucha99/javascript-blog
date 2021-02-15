083afa6b5427a5ad51abb96e163d4eb8f6aa2be6
Bartek Mucha <bartekmucha43@gmail.com>
2021-02-15 19:56:51 +0100

    Add class active - exercise

--- js/script.js
+++ js/script.js
@@ -6,21 +6,26 @@
 const titleClickHandler = function (event) {
   console.log("Link was clicked!");
   console.log(event);
-  /* remove class 'active' from all article links  */
+  /* [DONE] remove class 'active' from all article links  */
   const activeLinks = document.querySelectorAll(".titles a.active");
 
   for (let activeLink of activeLinks) {
     activeLink.classList.remove("active");
   }
 
-  /* add class 'active' to the clicked link */
+  /* [IN PROGRESS] add class 'active' to the clicked link */
+  const clickedElement = this;
+  clickedElement.classList.add("active");
 
-  /* remove class 'active' from all articles */
+  console.log("clickedElement:", clickedElement);
+
+  /* [DONE] remove class 'active' from all articles */
   const activeArticles = document.querySelectorAll(".posts .active");
 
   for (let activeArticle of activeArticles) {
     activeArticle.classList.remove("active");
   }
+
   /* get 'href' attribute from the clicked link */
 
   /* find the correct article using the selector (value of 'href' attribute) */
